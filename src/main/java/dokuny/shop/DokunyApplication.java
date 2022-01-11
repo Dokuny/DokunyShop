10bf20d2c540024448b85ad4b53bda5fe106e76c
Dokuny <nestour95@gmail.com>
2022-01-11 13:41:57 +0900

    init DokunyShop project

---
+++ src/main/java/dokuny/shop/DokunyApplication.java
@@ -0,0 +1,13 @@
+package dokuny.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DokunyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DokunyApplication.class, args);
+    }
+
+}
